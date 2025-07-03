c713144f01db5c4f335c6f0d1fc8c082b7d30065
cibere <71997063+cibere@users.noreply.github.com>
2025-07-03 15:49:45 -0700

    fix bug with new behavior in flow 1.20

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, "lib"))
 sys.path.append(os.path.join(parent_folder_path, ".venv", "lib", "site-packages"))
 
-from plugin.core import plugin  # noqa: E402
+from uvxpy.core import plugin  # noqa: E402
 
 plugin.run()
